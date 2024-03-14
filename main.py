a279d6cff2e02fe1103dfc23d0f75223b4c72d8a
Roy Moore <roy@moore.co.il>
2024-03-14 18:10:40 +0000

    Improve Testing

--- main.py
+++ main.py
@@ -2,13 +2,21 @@ import docker
 
 client = docker.from_env()
 
+def print_containers_status():
+    containers_list = client.containers.list()
+    print(f"Found {len(containers_list)} containers")
+    for container in containers_list:
+        print(container.name, container.status)
+
 test_container = client.containers.run("ubuntu", "echo hello world", detach=True)
 print(test_container.name, test_container.status)
 
 # List containers
-print("List(Pre):", client.containers.list())
+print("Pre:")
+print_containers_status()
 
 test_container.remove()
 
 # List containers
-print("Lis(Post):", client.containers.list())
+print("Post:")
+print_containers_status()
