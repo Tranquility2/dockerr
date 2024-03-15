988252571032e109f5f3781c7e28ff8769ecca5c
Roy Moore <roy@moore.co.il>
2024-03-15 16:14:46 +0000

    Update flow

--- main.py
+++ main.py
@@ -2,21 +2,18 @@ import docker
 
 client = docker.from_env()
 
-def print_containers_status():
+def print_containers_status(header: str = "Containers"):
     containers_list = client.containers.list()
-    print(f"Found {len(containers_list)} containers")
+    print(f"{header} Found {len(containers_list)} containers")
     for container in containers_list:
-        print(container.name, container.status)
+        print(f"-> {container.name} [{container.status}]")
 
 test_container = client.containers.run("ubuntu", "echo hello world", detach=True)
 print(test_container.name, test_container.status)
 
-# List containers
-print("Pre:")
-print_containers_status()
+
+print_containers_status(header="(Pre)")
 
 test_container.remove()
 
-# List containers
-print("Post:")
-print_containers_status()
+print_containers_status(header="(Post)")
