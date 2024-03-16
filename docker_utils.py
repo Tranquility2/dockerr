fd1c5691c954e7e3f0e18b584813c8d85b739f1a
Roy Moore <roy@moore.co.il>
2024-03-16 15:04:14 +0000

    Build image using direct API

--- docker_utils.py
+++ docker_utils.py
@@ -1,21 +1,41 @@
+import json
 import docker
 from docker.models.containers import Container
+from docker.models.images import Image
+
+from typing import Optional
 
 
 class DockerUtils:
     def __init__(self):
         self.client = docker.from_env()
 
-    def build_image(self, tag, path, dockerfile="Dockerfile") -> str:
+    def build_image(self, tag, path, dockerfile="Dockerfile") -> Optional[str]:
         print("Building image...")
-        image_object, image_logs = self.client.images.build(path=path,
-                                                            dockerfile=dockerfile,
-                                                            tag=tag)
+        # image_object, image_logs = self.client.images.build(path=path, dockerfile=dockerfile, tag=tag)
+        # Preffer direct API call to get live logs
+        live_logs = self.client.api.build(path=path,
+                                          dockerfile=dockerfile,
+                                          tag=tag,
+                                          decode=True)
 
-        for line in image_logs:
-            print(line)
+        image_id = None
 
-        return image_object.id
+        for line_dict in live_logs:
+            if line_dict.get("stream"):
+                print(line_dict["stream"], end="")
+            elif line_dict.get("error"):
+                print("Error:", line_dict["error"], end="")
+            elif line_dict.get("status"):
+                print(line_dict["status"], end="")
+            elif line_dict.get("aux"):
+                image_id = line_dict.get("aux").get("ID")
+            else:
+                print(json.dumps(line_dict, indent=4))
+
+        if image_id:
+            print(f"Image ID: {image_id}")
+            return image_id
 
     def run_container(self, image, detach, name, ports) -> Container:
         print("Running container...")
@@ -29,7 +49,7 @@ class DockerUtils:
         print("Getting container...")
         container = self.client.containers.get(container_id)
         return container
-    
+
     def print_logs(self, container: Container) -> None:
         logs = container.logs().decode("utf-8")
         print("Container Logs:\n", logs)
