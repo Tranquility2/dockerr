0c408d64358097c89f5dbcf4f425fedc6991b9ea
Roy Moore <roy@moore.co.il>
2024-03-16 14:32:36 +0000

    Minor fixes

--- main.py
+++ main.py
@@ -10,12 +10,13 @@ docker_utils.build_image(tag=TAG, path=".")
 
 try:
     test_container_obj = docker_utils.run_container(image=TAG,
-                                 detach=True,
-                                 name="test-container",
-                                 ports={"9000/tcp": 9000})
+                                                    detach=True,
+                                                    name="test-container",
+                                                    ports={"9000/tcp": 9000})
 
     try:
-        import time; time.sleep(0.3) # Python needs time to load
+        import time
+        time.sleep(0.5)  # Python needs time to load
         response = httpx.get(f"http://localhost:9000", timeout=5)
         print("Response:\n", response.text.partition('\n')[0])
     except Exception as e:
